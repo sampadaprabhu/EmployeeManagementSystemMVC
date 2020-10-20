5ef87af11a9c9a2b9fada3f99fa92be3dd8ce382
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-10-20 12:48:53 +0530

    [SAMPADA] ADD : Error Messages And Regex Patterns In Model Layer

--- EmpManagementML/EmpManagementModelLayer.cs
+++ EmpManagementML/EmpManagementModelLayer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,21 @@ namespace EmpManagementML
     {
         public int EmpID { get; set; }
 
+        [Required(ErrorMessage = "First Name Is Required")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Last Name Is Required")]
         public string LastName { get; set; }
 
+        [Required(ErrorMessage = "Email ID Is Required")]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Please Enter Valid Email ID")]
         public string EmailID { get; set; }
 
+        [Required(ErrorMessage = "Phone Number Is Required")]
+        [RegularExpression(@"^([6-9]{1}[0-9]{9})$", ErrorMessage = "Please Enter Valid Phone Number")]
         public string PhoneNumber { get; set; }
 
+        [Required(ErrorMessage = "Department ID Is Required")]
         public string DepartmentID { get; set; }
     }
 }
