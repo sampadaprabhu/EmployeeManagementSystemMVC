5507bcfe3cc58d353d0a92977bab3c704e43ea25
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-10-20 12:18:55 +0530

    [SAMPADA] ADD : Functionality To Add Employee In EmployeeManagementRepositoryLayer Class

--- EmpManagementRL/EmpManagementRepositoryLayer.cs
+++ EmpManagementRL/EmpManagementRepositoryLayer.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using EmpManagementML;
+using EmpManagementRL.implementation;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EmpManagementRL
 {
-    public class EmpManagementRepositoryLayer
+    public class EmpManagementRepositoryLayer : IEmpManagementRepositoryLayer
     {
+        static string connect = ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+        SqlConnection connection = new SqlConnection(connect);
+
+        public bool AddEmployee(EmpManagementModelLayer empManagementModelLayer)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand command = new SqlCommand("SPAddEmployeeData", this.connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@FirstName", empManagementModelLayer.FirstName);
+                    command.Parameters.AddWithValue("@LastName", empManagementModelLayer.LastName);
+                    command.Parameters.AddWithValue("@EmailID", empManagementModelLayer.EmailID);
+                    command.Parameters.AddWithValue("@PhoneNumber", empManagementModelLayer.PhoneNumber);
+                    command.Parameters.AddWithValue("@DepartmentID", empManagementModelLayer.DepartmentID);
+                    this.connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    this.connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
     }
 }
--- EmpManagementRL/interface/IEmpManagementRepositoryLayer.cs
+++ EmpManagementRL/interface/IEmpManagementRepositoryLayer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmpManagementML;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace EmpManagementRL.implementation
 {
     public interface IEmpManagementRepositoryLayer
     {
+        bool AddEmployee(EmpManagementModelLayer empManagementModelLayer);
     }
 }
