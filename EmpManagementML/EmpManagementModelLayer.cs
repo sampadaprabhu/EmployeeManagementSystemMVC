d26c0fde712aef24e6006feb906b921509bea491
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-10-20 12:03:10 +0530

    [SAMPADA] ADD : Entities In Model Layer Class

---
+++ EmpManagementML/EmpManagementModelLayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpManagementML
+{
+    public class EmpManagementModelLayer
+    {
+        public int EmpID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailID { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string DepartmentID { get; set; }
+    }
+}
