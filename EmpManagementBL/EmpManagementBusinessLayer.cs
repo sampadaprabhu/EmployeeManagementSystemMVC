64eb40bdccc6e18e564832d80a0a7cd129a764ce
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-10-20 12:45:38 +0530

    [SAMPADA] ADD : Functionalities In Business Layer

--- EmpManagementBL/EmpManagementBusinessLayer.cs
+++ EmpManagementBL/EmpManagementBusinessLayer.cs
@@ -1,12 +1,69 @@
-﻿using System;
+﻿using EmpManagementBL.implementation;
+using EmpManagementML;
+using EmpManagementRL.implementation;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EmpManagementBL
 {
-    public class EmpManagementBusinessLayer
+    public class EmpManagementBusinessLayer : IEmpManagementBusinessLayer
     {
+        private readonly IEmpManagementRepositoryLayer empManagementRepositoryLayer;
+
+        public EmpManagementBusinessLayer(IEmpManagementRepositoryLayer empManagementRepositoryLayer)
+        {
+            this.empManagementRepositoryLayer = empManagementRepositoryLayer;
+        }
+        public bool AddEmployee(EmpManagementModelLayer empManagementML)
+        {
+            try
+            {
+                return this.empManagementRepositoryLayer.AddEmployee(empManagementML);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public bool UpdateEmployee(EmpManagementModelLayer empManagementML)
+        {
+            try
+            {
+                return this.empManagementRepositoryLayer.UpdateEmployee(empManagementML);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public bool DeleteEmployee(string EmpID)
+        {
+            try
+            {
+                return this.empManagementRepositoryLayer.DeleteEmployee(EmpID);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public DataSet GetAllEmployee()
+        {
+            try
+            {
+                return this.empManagementRepositoryLayer.GetAllEmployee();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
     }
 }
--- EmpManagementBL/interface/IEmpManagementBusinessLayer.cs
+++ EmpManagementBL/interface/IEmpManagementBusinessLayer.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using EmpManagementML;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,12 @@ namespace EmpManagementBL.implementation
 {
     public interface IEmpManagementBusinessLayer
     {
+        bool AddEmployee(EmpManagementModelLayer empManagementML);
+
+        bool UpdateEmployee(EmpManagementModelLayer empManagementML);
+
+        bool DeleteEmployee(string EmpID);
+
+        DataSet GetAllEmployee();
     }
 }
